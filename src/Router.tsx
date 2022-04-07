98ec3a59eb6f42c0b1fc4b83fbf4c5c86c961138
raccoon-ccoder <qorwjddus96@naver.com>
2022-04-07 17:25:40 +0900

    라우트 설정 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ import Chart from "./routes/Chart";
 
 function Router({isDarkMode, toggleDarkMode}) {
     return(
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Coins isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />}/>
                 <Route path="/:coinId" element={<Coin isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />}>
